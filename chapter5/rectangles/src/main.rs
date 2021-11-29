eb299fa674d994a12c018aad388bfbb546b7c91e
Jacob Prud'homme <jacobprudhomme@users.noreply.github.com>
2021-11-29 16:31:18 -0500

    Completed chapter 5.3 - Method Syntax

--- chapter5/rectangles/src/main.rs
+++ chapter5/rectangles/src/main.rs
@@ -4,6 +4,34 @@ struct Rectangle {
     height: u32,
 }
 
+// We can split up these methods over several impl blocks. There's no need to here, but there is a use for this
+impl Rectangle {  // Everything within an implementation block is associated to the named type
+    // Methods are just like functions, but called within the context of a struct (or enum or trait), and their first parameter is always self (the struct instance)
+    // Their main benefit is code organization and discovery
+    fn area(&self) -> u32 {  // &self is short for self: &Self, where Self is an alias for the type the impl block is for
+        self.width * self.height
+    }
+
+    // We can have methods with the same name as fields, the language knows how to differentiate the two
+    // Methods with the same name as a field are usually defined to be getters (just return the field value). Rust does not automatically implement these
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        (self.width >= other.width) && (self.height >= other.height)
+    }
+
+    // Methods are a specific form of associated function (called as such because they're associate with the type the impl block represents) with self as the first parameter
+    // Here is a general associated function, that does not need an instance of Rectangle to act on
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+}
+
 fn main() {
     // Attempt 1:
     // let width = 30;
@@ -31,7 +59,28 @@ fn main() {
     // println!("The area of the rectangle is {} square pixels.", area(rect));
 
     // Attempt 3:
-    println!("The area of the rectangle is {} square pixels.", area(&rect));
+    println!("The area of the rectangle is {} square pixels.", area(&rect));  // Using function
+    println!("The area of the rectangle is {} square pixels.", rect.area());  // Using method
+    // Rust has automatic referencing and dereferencing for methods, i.e. rect automatically becomes &rect/&mut rect/* rect,
+    // depending on the type given to self in the method signature, so we don't have to write things like (&rect).area()
+
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let square = Rectangle::square(3);  // This is how we call associated functions
 }
 
 // Attempt 1:
